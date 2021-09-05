695b4d2f221596aba0e944fa602fd43728de2105
Beherith <mysterme@gmail.com>
2021-09-05 13:44:16 +0200

    progress check

--- var/plugins/barmanager.py
+++ var/plugins/barmanager.py
@@ -4,11 +4,37 @@ import traceback
 import sys
 import json
 import base64
+import zlib
+import os
+import time
 
 # perl.BarManager is the Perl representation of the BarManager plugin module
 # We will use this object to call the plugin API
 spads=perl.BarManager
 
+#------------------ Instance global "constants" -------------
+
+BMP = "BarManager|"
+
+SpringCrashInfologPatterns = ['Spring: caught content_error:'] # add any text here that we would like to catch as a crash, preferably, this should be in the barmanager config though
+CrashDir = ""
+
+DBGLEVEL = 3
+
+pluginParams = {}
+#---------------- Battleroom Variables to Track --------------
+AisPresent = {} # dunno the format yet, should support script tags to set AI profiles
+isBattleLocked = False
+BattleState = {}
+
+
+
+spadsConf = None #  {'lobbyReconnectDelay': 15, 'banList': 'empty', 'mapLink': 'http://springfiles.com/search_result.php?search=%m&select=select_all', 'promoteMsg': '%pplayer(s) needed for battle "%b" [%o, %a] (%u)', 'autoLock': 'off', 'lobbyPassword': 'petike', 'lobbyFollowRedirect': '1', 'lobbyHost': 'bar.teifion.co.uk', 'allowedLocalAIs': 'E323AI;HughAI;KAIK;RAI', 'logGameChat': '1', 'msgFloodAutoKick': '15;7', 'masterChannel': 'autohosts', 'allowGhostMaps': '0', 'maxBots': '16', 'lobbyInterfaceLogLevel': '5', 'opOnMasterChannel': '0', 'teamSize': 6, 'lobbyPort': '8200', 'commandsFile': 'commands.conf', 'unlockSpecDelay': '5;30', 'logPvChat': '1', 'extraBox': '0', 'alertDuration': '72', 'privacyTrustLevel': '130', 'springServer': '/home/eru/spads/var/spring/spring_bar_.BAR.104.0.1-1956-g0092498_linux-64-minimal-portable/spring-headless', 'simpleEventLogLevel': '5', 'springDataDir': '/home/eru/spads/var/spring/data', 'autoBlockColors': '0', 'autoManagedSpringDir': '/home/eru/spads/var/spring', 'rotationManual': 'random', 'rotationType': 'map;certified', 'defaultPreset': 'team', 'spoofProtection': 'warn', 'minPlayers': 1, 'welcomeMsgInGame': 'Hi %u (%d), welcome to %n .', 'shareId': '', 'autoCallvote': '1', 'ircColors': '0', 'voteMode': 'normal', 'autoStart': 'on', 'reCallVoteDelay': 10, 'springServerType': 'headless', 'promoteChannels': 'main', 'autoHostPort': '53199', 'welcomeMsg': 'Hi %u (%d), welcome to %n .', 'maxAutoHostMsgLength': '240', 'rotationEndGame': 'off', 'logBattleJoinLeave': '1', 'cmdFloodAutoIgnore': '8;8;4', 'pluginsDir': '/home/eru/spads/var/plugins', 'autoSpecExtraPlayers': '1', 'autoLearnMaps': '1', 'voteTime': 45, 'springConfig': '', 'description': 'Team Game Global Settings', 'advertDelay': '15', 'logChanChat': '0', 'kickFloodAutoBan': '5;120;5', 'dataDumpDelay': 60, 'clanMode': 'tag(5);pref(5)', 'nbTeams': 2, 'rotationEmpty': 'random', 'alertDelay': '6', 'lobbyLogin': '[teh]host15', 'botsRank': '3', 'hostingPreset': 'enginetesting', 'noSpecDraw': '0', 'autoManagedSpringVersion': '', 'battlePreset': 'team', 'autoAddBotNb': 0, 'freeSettings': 'autoLock;teamSize(1-8)', 'maxSpecs': '', 'colorSensitivity': 55, 'autoReloadArchivesMinDelay': 30, 'endGameAwards': '1', 'ghostMapLink': 'http://springfiles.com/search_result.php?search=%m&select=select_all', 'broadcastChannels': 'autohosts', 'maxChatMessageLength': 1024, 'logDir': '/home/eru/spads/var/spads_host15/log', 'autoBlockBalance': '1', 'autoHostInterfaceLogLevel': '5', 'hideMapPresets': '0', 'kickBanDuration': '300', 'maxBytesSent': 49000, 'logBattleChat': '1', 'useWin32Process': '0', 'map': 'Comet Catcher Remake 1.8', 'allowMapOptionsValues': '1', 'autoSaveBoxes': '2', 'voteRingDelay': '0', 'allowModOptionsValues': '1', 'eventModel': 'auto', 'promoteDelay': '600', 'maxLocalBots': '16', 'votePvMsgDelay': '0', 'maxRemoteBots': '16', 'statusFloodAutoKick': '24;8', 'userDataRetention': '-1;-1;-1', 'autoSetVoteMode': '1', 'logGameServerMsg': '1', 'instanceDir': '/home/eru/spads/var/spads_host15', 'maxLowPrioBytesSent': 48000, 'allowSettingsShortcut': '1', 'autoStop': 'gameOver', 'skillMode': 'TrueSkill', 'rankMode': 'account', 'localLanIp': '192.168.1.102', 'minTeamSize': '1', 'rotationDelay': 600, 'idShareMode': 'off', 'logChanJoinLeave': '0', 'forceHostIp': '', 'spadsLogLevel': '5', 'autoLockClients': 64, 'endGameCommandEnv': '', 'speedControl': '2', 'endGameCommand': '', 'autoLoadPlugins': 'BarManager;AutoRegister;JsonStatus;InGameMute', 'restoreDefaultPresetDelay': '30', 'springieEmulation': 'warn', 'logGameJoinLeave': '1', 'sendRecordPeriod': 5, 'noSpecChat': '0', 'autoLockRunningBattle': '0', 'balanceMode': 'clan;skill', 'updaterLogLevel': '5', 'varDir': '/home/eru/spads/var', 'midGameSpecLevel': '0', 'handleSuggestions': '0', 'unitsyncDir': '/home/eru/spads/var/spring/spring_bar_.BAR.104.0.1-1956-g0092498_linux-64-minimal-portable/', 'endGameCommandMsg': '', 'etcDir': '/home/eru/spads/etc', 'localBots': 'joe 0 E323AI;jim core#FF0000 KAIK', 'minRingDelay': '20', 'minVoteParticipation': '50', 'advertMsg': '', 'maxChildProcesses': '32', 'autoRestartForUpdate': 'off', 'forwardLobbyToGame': '1', 'nbPlayerById': 1, 'autoLoadMapPreset': '0', 'autoFixColors': 'advanced', 'preset': 'team', 'alertLevel': 130, 'autoBalance': 'advanced', 'maxSpecsImmuneLevel': '100', 'floodImmuneLevel': 100, 'autoUpdateRelease': '', 'mapList': 'all', 'autoUpdateDelay': '300'}
+
+
+#------------------ JSON OBJECT INFO ------------------
+# Each json string will contain a dict, for example, for a votestart
+#
 
 # This is the first version of the plugin
 pluginVersion='0.1'
@@ -20,7 +46,8 @@ requiredSpadsVersion='0.12.29'
 # We define 2 global settings (mandatory for plugins implementing new commands):
 # - commandsFile: name of the plugin commands rights configuration file (located in etc dir, same syntax as commands.conf)
 # - helpFile: name of plugin commands help file (located in plugin dir, same syntax as help.dat)
-globalPluginParams = None#{ 'commandsFile': ['BarManagerCmd.conf'],
+globalPluginParams = {'crashDir':['notNull'], 'crashFilePattern': ['notNull'], 'crashFilesToSave':['notNull'], 'crashInfologPatterns':['notNull'], 'chobbyGuiSettings':['notNull'], 'barManagerDebugLevel':['notNull']}
+					#{ 'commandsFile': ['BarManagerCmd.conf'],
 					  # 'helpFile': ['BarManagerHelp.dat'] }
 presetPluginParams = None
 
@@ -37,7 +64,28 @@ def getRequiredSpadsVersion(pluginName):
 def getParams(pluginName):
 	return [ globalPluginParams , presetPluginParams ]
 
+def json_gzip_base64(toencode):
+	return base64.b64encode(zlib.compress(json.dumps(toencode).encode("utf-8"))).decode()
 
+def SendBattleState():
+	try:
+		barmanagermessage = BMP + json.dumps({"BattleStateChanged": BattleState})
+		spads.sayBattle(barmanagermessage)
+		spads.slog(barmanagermessage, DBGLEVEL)
+	except Exception as e:
+		spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
+
+def BattleStateChanged(changedKey, changedValue):
+	try:
+		if changedKey not in BattleState or (changedKey in BattleState and BattleState[changedKey] != changedValue):
+			BattleState[changedKey] = changedValue
+			SendBattleState()
+		else:
+			spads.slog("No change in battle state: %s:%s"%(changedKey,changedValue),3)
+
+
+	except Exception as e:
+		spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 
 # This is the class implementing the plugin
 class BarManager:
@@ -56,6 +104,21 @@ class BarManager:
 			spads.slog("This plugin can fork processes",3)
 		if spads.get_flag("use_byte_string"):
 			spads.slog("This plugin can uses byte strings",3)
+
+		try:
+			pluginParams = spads.getPluginConf()
+			DBGLEVEL = int(pluginParams['barManagerDebugLevel'])
+			spadsConf = spads.getSpadsConf()
+			spads.slog("pluginParams = " + str(pluginParams),DBGLEVEL)
+			CrashDir = os.path.join(spadsConf['varDir'],'log',pluginParams['crashDir'])
+			if not os.path.exists(CrashDir):
+				spads.slog("Created CrashDir"+CrashDir,DBGLEVEL)
+				os.makedirs(CrashDir)
+			else:
+				spads.slog("OK:CrashDir already exists"+CrashDir,DBGLEVEL)
+
+		except Exception as e:
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 		
 		
 	# This is the callback called when the plugin is unloaded
@@ -76,33 +139,36 @@ class BarManager:
 			#spads.addTimer("yell",5, 0, lambda : spads.sayPrivate("[teh]Beherith","hi"))
 			
 			#dump stuff for testing
-			spadsdir = dir(spads)
-			spads.slog(str(spadsdir),3)
+			#spadsdir = dir(spads)
+			#spads.slog(str(spadsdir),3)
 			
-			confFull = spads.getSpadsConfFull()
-			spads.slog("maps=" + str(confFull.maps),3)
+			#confFull = spads.getSpadsConfFull()
+			#spads.slog("maps=" + str(confFull.maps),3)
 			
-			spads.slog(type(confFull),3)
-			spads.slog(str(confFull),3)
-			attrs = vars(confFull)
-			spads.slog(attrs,3)
-			attrs2 = dir(attrs)
-			spads.slog("attrdirdir:"+str(attrs2)+str(attrs.keys()),3)
-			for k in sorted(attrs.keys()):
-				spads.slog("%s:%s"%(str(k),str(attrs[k])),3)
-			spadsconf = spads.getSpadsConf()
-			spadsconf['map'] = "TheRock_V2"
+			#spads.slog(type(confFull),3)
+			#spads.slog(str(confFull),3)
+			#attrs = vars(confFull)
+			#spads.slog(attrs,3)
+			#attrs2 = dir(attrs)
+			#spads.slog("attrdirdir:"+str(attrs2)+str(attrs.keys()),3)
+			#for k in sorted(attrs.keys()):
+			#	spads.slog("%s:%s"%(str(k),str(attrs[k])),3)
+			#spadsconf = spads.getSpadsConf()
+			#spadsconf['map'] = "TheRock_V2"
 			a = None
 			#b = a + 1 # this is just a try:except: test
 			
-			miniconf = spads.getSpadsConf()
+			spadsConf = spads.getSpadsConf()
 			spads.slog("Trying to print spads configuration:",3)
-			spads.slog(str(miniconf),3)
+
+			#TODO: Init battlestatuschanged sanely!
+
+			#spads.slog(str(spadsConf),3)
 			
 			
 			
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 
 	def onGameEnd(self, endGameData):
 		# The \%endGameData parameter is a reference to a hash containing all the data stored by SPADS concerning the game that just ended.
@@ -110,29 +176,27 @@ class BarManager:
 		try:
 			spads.slog("onGameEnd",3)
 			spads.slog("endGameData" + str(endGameData),3)
-			GDRjson = json.dumps(endGameData).encode('utf-8')
-			b64 = base64.b64encode(GDRjson)
-			spads.sayPrivate('AutohostMonitor', b64)
+			spads.sayPrivate('AutohostMonitor', 'endGameData ' + json_gzip_base64(endGameData))
 			
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 	
 	def onJoinBattleRequest(self,userName,ipAddr):
 		# todo: send whole battle state to user
 		try:
 			spads.slog("onJoinBattleRequest:" + str(userName),3)
-			
+			SendBattleState() # TODO: not good, user doesnt get this yet (as he is not joined yet according to server!)
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 		return 0 # return 1 if the user isnt allowed to join (return string for reason)
 		
 	def onPresetApplied(self,oldPresetName,newPresetName):
 		# todo: send the updated preset to all battle participants
 		try:
 			spads.slog("onPresetApplied: " + str(oldPresetName) + " -> " + str(newPresetName),3)
-			
+			BattleStateChanged('preset',newPresetName)
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 		
 	def onPrivateMsg(self,userName,message):
 		# todo: nothing yet
@@ -140,7 +204,7 @@ class BarManager:
 			spads.slog("onPrivateMsg: " + str([userName,message]),3)
 			
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 		return 0 # return 1 if dont want Spads core to parse this message
 		
 	def onVoteRequest(self,source,user,command,remainingVoters):
@@ -152,15 +216,18 @@ class BarManager:
 			spads.slog("onVoteRequest: " + ','.join(map(str,[source,user,command,remainingVoters])), 3)
 			
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
+		return 1 # allow the vote, 0 for disallow
 			
 	def onVoteStart(self,user,command):
 		# command is an array reference containing the command for which a vote is started
 		try:
 			spads.slog("onVoteStart: " + ','.join(map(str,[user,command])), 3)
-			
+			barmanagermessage = BMP+json.dumps({"onVoteStart" : {'user':user, 'command':command}})
+			spads.sayBattle(barmanagermessage)
+			spads.slog(barmanagermessage,3)
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 	
 	def onVoteStop(self,voteResult):
 		# This callback is called each time a vote poll is stoped.
@@ -169,22 +236,34 @@ class BarManager:
 			spads.slog("onVoteStop: voteResult=" + str(voteResult), 3)
 			
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 
 	
 	def onSpringStop(self,springPid):
 		# This callback is called each time the Spring process ends.
 		try:
 			spads.slog("onSpringStop: springPid=" + str(springPid),3)
+			instanceDir = spadsConf["instanceDir"]
+			# Check for spring has crashed, and save the script and the infolog.txt
+			infologlines = open(instanceDir + "/log/infolog.txt").readlines()
+			for line in infologlines:
+				for pattern in pluginParams['crashInfologPatterns'].split('|'):
+					if pattern in line:
+						for crashFileToSave in pluginParams['crashFilesToSave'].split('|'):
+							outf = open(os.path.join(CrashDir, pluginParams['crashFilePattern']%(int(time.time()), crashFileToSave)),'w')
+							outf.write(''.join(open(os.path.join(instanceDir,'log',crashFileToSave)).readlines()))
+							outf.close()
+
+						break
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 	
 	def onSpringStart(self,springPid):
 		# This callback is called each time a Spring process is launched to host a game.
 		try:
 			spads.slog("onSpringStart: springPid=" + str(springPid),3)
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 	
 	def preGameCheck(self,force,checkOnly,automatic):
 		# This callback is called each time a game is going to be launched, to allow plugins to perform pre-game checks and prevent the game from starting if needed.
@@ -196,7 +275,7 @@ class BarManager:
 		try:
 			spads.slog("preGameCheck: " + ','.join(map(str,[force,checkOnly,automatic])),3)
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 		return None #
 	
 	def postSpadsCommand(self,command,source,user,params,commandResult):
@@ -209,8 +288,32 @@ class BarManager:
 		# todo: say the results of the changed stuff in battle room
 		try:
 			spads.slog("postSpadsCommand: " + ','.join(map(str,[command,source,user,params,commandResult])),3)
+			if command == "lock" :
+				BattleStateChanged("locked","on")
+			elif command == "unlock":
+				BattleStateChanged("locked","off")
+			elif command == "set": # autoLock|autoStart|autoBalance|autoFixColors|nbTeams|balanceMode|clanMode|locked|preset
+				lowercommand = params[0].lower()
+				if lowercommand == 'autobalance':
+					BattleStateChanged("autoBalance", params[1])
+				elif lowercommand == 'balancemode':
+					BattleStateChanged("balanceMode", params[1])
+				elif lowercommand == 'nbteams':
+					BattleStateChanged("nbTeams", params[1])
+				elif lowercommand == 'teamsize':
+					BattleStateChanged("teamSize", params[1])
+
+
+
+			elif command == "vote":
+
+				if params[0] in ['yes','y','no','n','blank','b']:
+					if params[0] == 'yes':
+						pass
+
+
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 		return None #
 
 	def preSpadsCommand(self,command,source,user,params):
@@ -223,7 +326,7 @@ class BarManager:
 		try:
 			spads.slog("preSpadsCommand: " + ','.join(map(str,[command,source,user,params])),3)
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 		return 1
 	
 	def filterRotationMaps(self,rotationMaps):
@@ -232,7 +335,7 @@ class BarManager:
 			#for map in rotationMaps:
 			#	spads.slog(str(str(map)),3)
 		except Exception as e:
-			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()))
+			spads.slog("Unhandled exception: " + str(sys.exc_info()[0]) + "\n" + str(traceback.format_exc()),0)
 		
 		return rotationMaps # The callback must return a reference to a new array containing the filtered map names
 
