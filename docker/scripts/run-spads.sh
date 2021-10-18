f86e7a6c38ee4035945be356f4da528f1a94038b
Amadeus Folego <amadeusfolego@gmail.com>
2021-10-18 18:59:19 -0300

    Wait for all instances to exit on termination

--- docker/scripts/run-spads.sh
+++ docker/scripts/run-spads.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 _term() {
-  echo "Caught SIGTERM signal!"
+  echo "Caught termination signal, passing to child process"
   kill -TERM "$child" 2>/dev/null
+
+  pids=`find /opt/spads/var/ClusterManager -type f -name *.pid -print | xargs -I{} sh -c "cat {}; echo ''"`
+  for pid in $pids
+  do
+    echo "> Waiting for instance with pid $pid to exit"
+    tail --pid=$pid -f /dev/null
+  done
+  echo "Done, exiting"
 }
 
 trap _term SIGTERM
+trap _term SIGINT
 
 cp -R /spads_etc/* /opt/spads/etc
 cp -R /spads_var/* /opt/spads/var
@@ -13,6 +22,15 @@ cp -R /spads_var/* /opt/spads/var
 mkdir -p /opt/spads/var/log
 mkdir -p /opt/spads/var/plugins
 mkdir -p /opt/spads/var/spring
+
+pidfiles=`find /opt/spads/var/ClusterManager -type f -name *.pid -print`
+if [[ ! -z "$pidfiles" ]]
+then
+  echo "Found stale pids:"
+  printf "$pidfiles\n"
+  echo "Cleaning up"
+  printf "$pidfiles" | xargs -I{} rm {}
+fi
 
 perl /opt/spads/spads.pl /opt/spads/etc/spads_cluster.conf \
   CMD_lobbyHost=$SPADS_LOBBY_HOST \
@@ -30,7 +48,10 @@ perl /opt/spads/spads.pl /opt/spads/etc/spads_cluster.conf \
   CMD_targetSpares=$SPADS_TARGET_SPARES \
   CMD_clusters=$SPADS_CLUSTER_PRESETS \
   CMD_endGameCommandPath=/opt/upload_replay.sh \
-  $SPADS_ARGS
+  $SPADS_ARGS &
 
 child=$!
+
+echo "Captured pid: $child"
+
 wait "$child"
