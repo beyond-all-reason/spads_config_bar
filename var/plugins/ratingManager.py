95f6fb8935bdf69bfd4f334334490d3549af5956
Teifion <teifion@teifion.co.uk>
2022-08-01 13:08:57 +0100

    Added uncertainty (#46)

--- var/plugins/ratingManager.py
+++ var/plugins/ratingManager.py
@@ -3,7 +3,7 @@ import json
 import perl
 spads = perl.RatingManager
 
-pluginVersion = '0.1'
+pluginVersion = '0.2'
 requiredSpadsVersion = '0.12.29'
 
 host_url = "https://server2.beyondallreason.info/teiserver/api/spads/get_rating"
@@ -20,13 +20,12 @@ class RatingManager:
 
     def updatePlayerSkill(self, playerSkill, accountId, modName, gameType):
         try:
-            with urllib.request.urlopen(f"{host_url}/{accountId}/{accountId}/{gameType}") as f:
+            with urllib.request.urlopen(f"{host_url}/{accountId}/{gameType}") as f:
                 raw_data = f.read().decode('utf-8')
                 data = json.loads(raw_data)
-                rating = data["rating"]
 
-                return [1, rating]
+                return [1, data["rating"], data["uncertainty"]]
         except Exception as e:
             spads.slog("Unhandled exception: " + str(sys.exc_info()
                        [0]) + "\n" + str(traceback.format_exc()), 0)
-            return [1, 24.999]
+            return [1, 24.999, 8.3333]
