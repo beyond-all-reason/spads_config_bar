d3426ba80c4a59c2a8b13ad8c25d702eff941a44
Beherith <mysterme@gmail.com>
2023-01-09 10:02:19 +0100

    Create setcpuaffinity.sh

---
+++ setcpuaffinity.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Sets the CPU affinity of all SPADS and spring-dedicated processes to all cpus every 30 minuts
+
+while true
+do
+	date -u
+	pgrep perl | xargs -L1 taskset -cp 0-7
+	pgrep spring | xargs -L1 taskset -cp 0-7
+	sleep 1800
+done 
